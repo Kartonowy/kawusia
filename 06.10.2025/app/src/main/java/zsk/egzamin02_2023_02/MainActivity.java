b7e5498dd74468d7aed41d27c151646b3cbb0962
Kartonowy <ttrollface.com.pl@gmail.com>
2025-10-23 09:37:53 +0200

    new

---
+++ 06.10.2025/app/src/main/java/zsk/egzamin02_2023_02/MainActivity.java
@@ -0,0 +1,67 @@
+package zsk.egzamin02_2023_02;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] options = {"Dzień dobry", "Good morning", "Buenos Dias" };
+    int current = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button arrow = (Button) findViewById(R.id.arrow);
+        SeekBar  seekBar = (SeekBar) findViewById(R.id.seekbar);
+        TextView cytat = (TextView) findViewById(R.id.cytat);
+        TextView rozmiar = (TextView) findViewById(R.id.size);
+
+        cytat.setText(options[current]);
+
+        arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (current == 2) {
+                    current = 0;
+                }
+                current += 1;
+                cytat.setText(options[current]);
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                rozmiar.setText("Rozmiar: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
